a3247596cbc84080a410ed801073e2d036f90531
Sneha  K P (LT) <snehakp@lockthreat.com>
2026-07-16 17:19:00 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your backend API URL
+ baseURL: import.meta.env.VITE_API_URL, // Replace with your backend API URL
 });
 
 export default api;
